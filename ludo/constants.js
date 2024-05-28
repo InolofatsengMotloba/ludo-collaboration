f79652084c4f29f8a340797e7de73bfaa66c7eb5
OfentseXI <dofentse69@gmail.com>
2024-05-28 11:29:28 +0200

    creation of all necessary constants

--- ludo/constants.js
+++ ludo/constants.js
@@ -85,3 +85,39 @@ export const COORDINATES_MAP = {
     602: [10.5, 3.45],
     603: [12.54, 3.45],
 };
+
+export const STEP_LENGTH = 6.66;
+
+export const PLAYER = ['P1' , 'P2'];
+
+export const BASE_POSITIONS = {
+    P1: [500, 501, 502, 503],
+    P2: [600, 601, 602, 603],
+}
+
+export const START_POSITIONS = {
+    P1: 0,
+    P2: 26
+}
+
+export const HOME_ENTRANCE = {
+    P1: [100, 101, 102, 103],
+    P2: [200, 201, 202, 203],
+}
+
+export const HOME_POSITIONS = {
+    P1: 105,
+    P2: 205
+}
+
+export const TURNING_POINTS = {
+    P1: 50,
+    P2: 24
+}
+
+export const SAFE_POSITIONS = [0, 8, 13, 21, 26, 34, 39, 47];
+
+export const STATE = {
+    DICE_NOT_ROLLED: 'DICE_NOT_ROLLED',
+    DICE_ROLLED: 'DICE_ROLLED',
+}
