e22cab4ed4b3155c50377a235b13bec7311f706f
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-03 14:50:23 +0200

    Added eventlisteners for dice,reset buttons and player piece elements

--- ludo/UI.js
+++ ludo/UI.js
@@ -1,4 +1,13 @@
 const diceButtonElement = document.querySelector ('#dice-btn');
 export class UI {
+        static listenDiceClick(callback) {
+                diceButtonElement.addEventListener('click', callback);
+        }
         
+        static listenResetClick(callback) {
+                document.querySelector('button#reset-btn').addEventListener('click', callback);
+        }
+        static listenPieceClick(callback) {
+                document.querySelector('.player-pieces').addEventListener('click', callback);
+        }
 }
