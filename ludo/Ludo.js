2f981a2d02477e9269023d0a6eba1d158f487ea3
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-04 14:42:38 +0200

    Property to store the current pos of all pieces

--- ludo/Ludo.js
+++ ludo/Ludo.js
@@ -1,6 +1,10 @@
 import {UI} from './UI.js'
 
 export class Ludo {
+        currentPositions = {
+                P1: [],
+                P2: []
+        }
         constructor () {
                 console.log('Hello World! Lets play Ludo');
         }
