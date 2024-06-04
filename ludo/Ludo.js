d2928635948277bc0ae1cf9ac22bd49626f3f3b0
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-04 15:39:26 +0200

    Funvtion to set State

--- ludo/Ludo.js
+++ ludo/Ludo.js
@@ -22,9 +22,24 @@ export class Ludo {
                this. _turn = value;
                UI.setTurn(value);
         }
+        _state;
+        get state() {
+                return this._state;
+        }
+        set state(value) {
+                this._state = value;
+
+                if(value === STATE.DICE_NOT_ROLLED) {
+                        UI.enableDice();
+                } else {
+                        UI.disableDice();
+                }
+        }
         constructor () {
                 console.log('Hello World! Lets play Ludo');
-                this.diceValue = 4;
-                this.turn = 0;
+                
+                // this.diceValue = 4;
+                // this.turn = 0;
+                // this.state = STATE.DICE_ROLLED;
         }
 }
