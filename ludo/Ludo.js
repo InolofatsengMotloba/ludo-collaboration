ed13e1e00bfd6e8ed38a649cb2bbca680342138b
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-04 15:16:17 +0200

    Setting the "turn" state

--- ludo/Ludo.js
+++ ludo/Ludo.js
@@ -5,7 +5,26 @@ export class Ludo {
                 P1: [],
                 P2: []
         }
+        diceValue;
+        get diceValue() {
+                return thid._diceValue;
+        }
+        set diceValue(value) {
+                this._diceValue = value;
+
+                UI.setDiceValue(value);
+        }
+        _turn;
+        get turn() {
+                return this._turn;
+        }
+        set turn(value) {
+               this. _turn = value;
+               UI.setTurn(value);
+        }
         constructor () {
                 console.log('Hello World! Lets play Ludo');
+                this.diceValue = 4;
+                this.turn = 0;
         }
 }
