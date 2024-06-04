36ed323b31b3a87f507940996b28ce21fe669277
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-04 10:54:46 +0200

    System update (Prettier)

--- ludo/UI.js
+++ ludo/UI.js
@@ -1,38 +1,46 @@
-import { COORDINATES_MAP } from './constants.js'
+import { COORDINATES_MAP } from "./constants.js";
 
-const diceButtonElement = document.querySelector ('#dice-btn');
+const diceButtonElement = document.querySelector("#dice-btn");
 const playerPiecesElements = {
-        P1: document.querySelectorAll('[player-id="P1"].player-piece'),
-        P2: document.querySelectorAll('[player-id="P2"].player-piece'),
-}
+  P1: document.querySelectorAll('[player-id="P1"].player-piece'),
+  P2: document.querySelectorAll('[player-id="P2"].player-piece'),
+};
 export class UI {
-        static listenDiceClick(callback) {
-                diceButtonElement.addEventListener('click', callback);
-        }
-        
-        static listenResetClick(callback) {
-                document.querySelector('button#reset-btn').addEventListener('click', callback);
-        }
-        static listenPieceClick(callback) {
-                document.querySelector('.player-pieces').addEventListener('click', callback);
-        }
+  static listenDiceClick(callback) {
+    diceButtonElement.addEventListener("click", callback);
+  }
 
-         /**
-         * 
-         * @param {string} player
-         * @param {Number} piece
-         * @param {Number} newPosition
-         */
-         setPiecePosition(player, piece, newPosition) {
-                if(!playerPiecesElements[player] || !playerPiecesElements[player][piece]) {
-                        console.error(`Player element of given player: ${player} and piece ${piece} not found`);
-                        return;
-                }
-               const[x, y] = COORDINATES_MAP[newPosition];
+  static listenResetClick(callback) {
+    document
+      .querySelector("button#reset-btn")
+      .addEventListener("click", callback);
+  }
+  static listenPieceClick(callback) {
+    document
+      .querySelector(".player-pieces")
+      .addEventListener("click", callback);
+  }
 
-               const pieceElement = playerPiecesElements[player][piece];
-               pieceElement.style.top = y * STEP_Length + '%';
-               pieceElement.style.left = x * STEP_Length + '%';
+  /**
+   *
+   * @param {string} player
+   * @param {Number} piece
+   * @param {Number} newPosition
+   */
+  static setPiecePosition(player, piece, newPosition) {
+    if (!playerPiecesElements[player] || !playerPiecesElements[player][piece]) {
+      console.error(
+        `Player element of given player: ${player} and piece ${piece} not found`
+      );
+      return;
+    }
 
-         }
+    const [x, y] = COORDINATES_MAP[newPosition];
+
+    const pieceElement = playerPiecesElements[player][piece];
+    pieceElement.style.top = y * STEP_Length + "%";
+    pieceElement.style.left = x * STEP_Length + "%";
+  }
 }
+
+UI.setPiecePosition("P1", 0, 0);
